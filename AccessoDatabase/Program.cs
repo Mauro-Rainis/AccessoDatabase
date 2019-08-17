72ee45ea6f5aa03f172e3380570c9da7ae0974e8
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-17 09:16:05 +0200

    Add project files.

---
+++ AccessoDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccessoDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
