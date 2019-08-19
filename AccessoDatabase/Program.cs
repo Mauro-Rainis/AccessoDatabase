43e1c932d4882f9cafb30944c439f80c34afd8d6
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-19 15:30:42 +0200

    Accesso a SqlServer

--- AccessoDatabase/Program.cs
+++ AccessoDatabase/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Data.SqlClient;
 
 namespace AccessoDatabase
 {
@@ -6,7 +7,25 @@ namespace AccessoDatabase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**** SqlServer connection"); 
+
+            // Connessione a SQLServer
+            var connStringSqlServer = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\mauro\\source\\repos\\_da_samsung\\AccessoDatabase\\AccessoDatabase\\BearziPersone.mdf;Integrated Security=True";
+            using (var conn = new SqlConnection(connStringSqlServer))
+            {
+                conn.Open();
+                var sql = "select * from persone";
+                SqlCommand sqlCmd = new SqlCommand(sql, conn);
+                SqlDataReader dr = sqlCmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("Id:{0} Cognome:{1} Nome:{2}",dr[0], dr[1], dr[2]);
+                    }
+                }
+            }
+
         }
     }
 }
