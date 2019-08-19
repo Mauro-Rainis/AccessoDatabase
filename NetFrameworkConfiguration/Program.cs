1c042dc69950d73a033969e5a5ade5403f1f9a2c
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-19 16:27:09 +0200

    Aggiunto nuovo progetto

---
+++ NetFrameworkConfiguration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var segreto = "Stringa segreta che non voglio vedere su git ma voglio modificare a Runtime";
+
+            Console.WriteLine("La stringa segreta è {0}", segreto);
+
+            // La modifica della stringa richiede la ri-compilazione del programma
+            // La stringa è presente nei sorgenti e quindi condivisa su git
+
+        }
+    }
+}
