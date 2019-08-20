a79515dc7654c7e1d5c5d2297b536ad5332f96fd
Mauro Rainis <mauro.rainis@gmail.com>
2019-08-20 17:21:15 +0200

    Aggiunto una chave "segreto" ad appsettings.json e mostro il valore nella pagina Index

--- AspNetCoreWebAppRazor/Pages/Index.cshtml.cs
+++ AspNetCoreWebAppRazor/Pages/Index.cshtml.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetCoreWebAppRazor.Pages
 {
     public class IndexModel : PageModel
     {
+        public readonly IConfiguration _configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
 
